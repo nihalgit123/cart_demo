f0aef4095e9462294bd9def04b040e73f7fbfa20
nihalgit123 <nihalchelembra@gmail.com>
2024-08-04 12:10:48 +0530

    provider firstcommit

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'provider demo',
       debugShowCheckedModeBanner: false,
       home: SplashScreen(),
     );
